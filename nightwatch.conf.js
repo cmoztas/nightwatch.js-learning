1c384b00a49739abd4d906131ade8091ec1ffefb
Çağkan Mert Öztaş <cmoztas2@gmail.com>
2022-08-27 21:41:53 +0300

    initial commit

---
+++ nightwatch.conf.js
@@ -0,0 +1,23 @@
+module.exports = {
+  src_folders: ['tests'],
+
+  webdriver: {
+    start_process: true,
+    port: 9515,
+    server_path: require('chromedriver').path,
+    cli_args: []
+  },
+
+  test_settings: {
+    default: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+          args: [
+            'start-maximized'
+          ]
+        }
+      }
+    }
+  }
+}
