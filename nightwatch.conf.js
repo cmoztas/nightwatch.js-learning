0ab6a96272238126a709324f6f60647d972d5536
Çağkan Mert Öztaş <cmoztas2@gmail.com>
2022-08-30 22:07:13 +0300

    asdfasdfasdfasdfasdfasdfasdfasdfasdf

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -4,10 +4,10 @@ module.exports = {
 
   webdriver: {
     start_process: true,
-    port: 9516,
+    port: 9223,
     server_path: 'node_modules/.bin/chromedriver',
     cli_args: [
-      '--port=9516'
+      '--port=9223'
     ]
   },
 
